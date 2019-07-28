1027df605a87d9f491edc4beb6f69e69f4184bdb
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-28 21:35:03 +0200

    Feat: API JS

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 const list = document.querySelector(".list--js");
+
+fetch('https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc')
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            list.innerHTML += `<li><a href="${repo.html_url}"> ${repo.name}</a></li>`;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+console.log('test 2');
