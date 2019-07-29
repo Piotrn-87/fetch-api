a567cc9c6b949bee963cb47afa9a2d29e110c937
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-29 19:01:35 +0200

    Feat: Added style

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ fetch('https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc'
     .then(resp => {
         const repos = resp;
         for (const repo of repos) {
-            list.innerHTML += `<li><a href="${repo.html_url}"> ${repo.name}</a></li>`;
+            list.innerHTML +=
+                `<li class="list__item">
+            <a class = "list__link" 
+            href="${repo.html_url}"> ${repo.name}</a></li>`;
         }
     })
     .catch(err => {
         console.log(err);
     })
-console.log('test 2');
