fd73ccc6aededd565a8e3c44c8f09235b8018fb4
Piotr Napieralski <piotrn-87@o2.pl>
2019-08-24 15:29:56 +0200

    Chore: Added JS and styling project

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const title = document.querySelector(".form__select-title--js");
+const userName = document.querySelector(".form__input-name--js");
+const direction = document.querySelector(".form__select-direction--js");
+const submit = document.querySelector(".button__submit--js");
 const headerText = document.querySelector('.header__title--js');
 const list = document.querySelector(".list--js");
 
@@ -16,18 +20,33 @@ const addLetter = () => {
 }
 const headerTyping = setInterval(addLetter, time);
 
-fetch('https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc')
-  .then(resp => resp.json())
-  .then(resp => {
-    const repos = resp;
-    for (const repo of repos) {
-      list.innerHTML +=
-        `<li class="list__item">
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = userName.value;
+  let sort = direction.value;
+
+  fetch(`https://api.github.com/users/${inputValue}/repos?sort=updated&direction=${sort}`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const repos = resp;
+      for (const repo of repos) {
+        list.innerHTML +=
+          `<li class="list__item">
             <a class = "list__link" 
             href="${repo.html_url}"> ${repo.name}</a></li>`;
-    }
-  })
+      }
+    })
+    .catch(err => {
+      console.log("cant find github API");
+    })
+})
 
-  .catch(err => {
-    console.log(err);
-  })
+const userNotFound = (inputValue) => {
+  let resp = `<li class ="project">
+<div class="project__wrapper">
+<img class="project__logo" src="assets/img/github.svg" alt="github image">
+<h3 class="project__name">${inputValue} doesent exist</h3> 
+</div>
+</li>`;
+  list.innerHTML = resp;
+}
